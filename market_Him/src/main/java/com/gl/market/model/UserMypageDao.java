18dcd00cbc406de7deb1c0715c4be856b1c02b44
HB6017\user <chkmkm458.gmail.com>
2016-12-21 17:06:36 +0900

    12.21 장바구니 조회,삭제 완료

---
+++ market_Him/src/main/java/com/gl/market/model/UserMypageDao.java
@@ -0,0 +1,23 @@
+package com.gl.market.model;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface UserMypageDao {
+
+	UserJoinVo userOne(String id);
+	UserJoinVo mileCk(String id);
+	int copCk(String id);
+	List<UserCopVo> copList(HashMap<String,Object> map);
+	void userUpdata(UserJoinVo data);
+	void userDelete(String id);
+	int jumunCk(String id);
+	List<UserOrderVo> juList(HashMap<String, Object> map);
+	void juCancel(String orderid);
+	int cancelCk(String id);
+	List<UserOrderVo> cnlList(HashMap<String, Object> map);
+	int basketCk(String id);
+	List<UserOrderVo> bkList(HashMap<String, Object> map);
+	void bkCancel(String basketid);
+	
+}
